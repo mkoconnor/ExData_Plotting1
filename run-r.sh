d3820af93c51e68cfb844c2ae004dfbf9b97c36b
Michael O'Connor <mkoconnor@gmail.com>
2014-12-06 11:00:42 -0500

    filter file in bash first

--- run-r.sh
+++ run-r.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# Filter dataset to just dates we're interested for efficiency.  It's
+# also filtered again in the R code, which won't hurt (I started
+# filtering there and just left it in).
+
+unfiltered_file=~/Downloads/household_power_consumption.txt
+
+( head -n 1 ${unfiltered_file}; grep -E '^[12]/2/2007;' ${unfiltered_file} ) > filtered_power_consumption.txt
+
 for file in $(dirname $0)/plot*.R; do
     R --vanilla --slave < $file
 done
