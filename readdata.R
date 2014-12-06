624dc6c3b91364aef0cb5895baacc089560d9436
Michael O'Connor <mkoconnor@gmail.com>
2014-12-06 11:12:21 -0500

    try putting the right times on

--- readdata.R
+++ readdata.R
@@ -1,4 +1,4 @@
 data <- read.table("filtered_power_consumption.txt",sep=";",header=TRUE,na.strings="?")
 data$Date <- as.Date(data$Date,format="%d/%m/%Y")
 data <- data[data$Date==as.Date("2007-02-01") | data$Date==as.Date("2007-02-02"),]
-data$Time <- strptime(data$Time,format="%H:%M:%S")
+data$Time <- mapply((function(d,t) strptime(paste(as.character(d), " ",t), format="%Y-%m-%d %H:%M:%S")),data$Date,data$Time, SIMPLIFY=FALSE)
