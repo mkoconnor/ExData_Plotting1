51086c5e59eac8ad7bc15bd8fbfcc459159289c8
Michael O'Connor <mkoconnor@gmail.com>
2014-12-06 10:50:38 -0500

    fix ylab

--- plot1.R
+++ plot1.R
@@ -5,6 +5,6 @@ data$Time <- strptime(data$Time,format="%H:%M:%S")
 
 png(filename="plot1.png", width=480, height=480)
 
-hist(data$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
+hist(data$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)", ylab="Frequency")
 
 dev.off()
